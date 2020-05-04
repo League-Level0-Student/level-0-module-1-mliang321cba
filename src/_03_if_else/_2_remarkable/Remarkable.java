2b986c281dbe09e525037a7ec1141dedd737df60
root <root@ed0485562e18>
2020-05-04 20:28:49 +0000

    first time programming with a group!

--- src/_03_if_else/_2_remarkable/Remarkable.java
+++ src/_03_if_else/_2_remarkable/Remarkable.java
@@ -8,11 +8,19 @@ public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
-
+		String Maggie = "happy";
+		String Michelle = "nice";
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+		String input = JOptionPane.showInputDialog("Enter a name.");
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
+		if(input.equalsIgnoreCase("Maggie")) {
 
+		JOptionPane.showMessageDialog(null, "Maggie is a " + Maggie + " girl.");
+	
+		}
+		if(input.equalsIgnoreCase("Michelle")){
+			JOptionPane.showMessageDialog(null, "Michelle is a " + Michelle + " girl.");
+		}
 	}
 }
 
---
+++ src/_05_for_loops/_2_badgers/Badgers.java
@@ -0,0 +1,31 @@
+package _05_for_loops._2_badgers;
+
+public class Badgers {
+	public static void main(String[] args) {
+		String word1 = "badger";
+		String word2 = "mushroom";
+		String word3 = "snake";
+			for (int i = 0; i < 10; i++) {
+				System.out.println(word1);
+			}	
+			System.out.println(word2 + " " + word2 + " a");
+			
+			for (int i = 0; i < 10; i++) {
+				System.out.println(word1);
+			}	
+			System.out.println(word2 + " " + word2 + " a");
+			
+			for (int i = 0; i < 10; i++) {
+				System.out.println(word1);
+			}	
+			System.out.println("Mush-" + word2 + " a");
+			for (int i = 0; i < 10; i++) {
+				System.out.println(word1);
+			}
+			System.out.println("a " + word3 + ", a " + word3);
+			System.out.println(word3 + "! A " + word3 + ", ooh, it's a " + word3 + "!");
+			
+		}
+	
+}
+
